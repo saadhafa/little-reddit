949ec8f334dd6ea07939b6864af446e9cdc95243
saadhafa <saad.811@icloud.com>
2021-04-29 16:54:45 +0000

    now user can use UI form to send email to change password

--- frontend-app/src/pages/login.tsx
+++ frontend-app/src/pages/login.tsx
@@ -1,7 +1,7 @@
 
 import React from 'react'
 import {Form, Formik} from 'formik'
-import { Button } from '@chakra-ui/react';
+import { Button,Link } from '@chakra-ui/react';
 import Wrapper from '../components/wrapper';
 import InputField from '../components/InputFiled';
 import { useLoginMutation } from '../generated/graphql';
@@ -9,6 +9,8 @@ import { errorMap } from '../util/errorMap';
 import {useRouter} from 'next/router'
 import { withUrqlClient } from 'next-urql';
 import { CreateUrqlClient } from '../util/createUrqlClient';
+import NextLink from 'next/link';
+
 
 
 
@@ -41,17 +43,16 @@ import { CreateUrqlClient } from '../util/createUrqlClient';
         <Form>
           <InputField name="usernameOrEmail" label="username or email" placeholder="username or email"  />
           <InputField name="password" label="password" placeholder="password" type="password"  />
-          <Button mt={5} type="submit" colorScheme="teal">Login</Button>
+          <Button mb={4} mt={5} type="submit" colorScheme="teal">Login</Button>
         </Form>
 
       )}
 
-  
-
-
 
   </Formik>  
-
+  <NextLink href="/forgot-password">
+  <Link>Forgot password</Link>
+</NextLink>
   </Wrapper>  
 
 
--- frontend-app/src/pages/reset-password/[token].tsx
+++ frontend-app/src/pages/reset-password/[token].tsx
@@ -1,4 +1,4 @@
-import { Alert, AlertIcon, Button } from "@chakra-ui/react";
+import { Alert, AlertIcon, Button, Link } from "@chakra-ui/react";
 import { Form, Formik } from "formik";
 import { NextPage, NextPageContext } from "next";
 import { NextComponentType, withUrqlClient } from "next-urql";
@@ -10,6 +10,7 @@ import Wrapper from "../../components/wrapper";
 import { useChangePasswordMutation } from "../../generated/graphql";
 import { CreateUrqlClient } from "../../util/createUrqlClient";
 import { errorMap } from "../../util/errorMap";
+import NextLink from 'next/link'
 
 
 
@@ -20,6 +21,12 @@ const ResetPassword:NextPage<{token:string}> = ({token}) => {
     const [tokenErr,setTokenErr] = useState<string | null>(null)
 
     const [,changePassword] = useChangePasswordMutation()
+
+
+   
+ 
+        
+    
 
     return (
         <>
@@ -32,7 +39,10 @@ const ResetPassword:NextPage<{token:string}> = ({token}) => {
         tokenErr ?  
             <Alert status="error">
                 <AlertIcon />
-                {tokenErr}
+                {tokenErr}        
+                <NextLink href="/forgot-password">
+                    <Link ml={2} color-="red">Go to forgot password</Link>
+                </NextLink>
             </Alert> 
         
             : null
@@ -59,12 +69,13 @@ const ResetPassword:NextPage<{token:string}> = ({token}) => {
       
               <Form>
                 <InputField name="newPassword" type="password" label="new password" placeholder="new password"  />
-                <Button isLoading={isSubmitting}  mt={5} type="button" colorScheme="teal">Change password</Button>
+                <Button mb={10} isLoading={isSubmitting}  mt={5} type="button" colorScheme="teal">Change password</Button>
               </Form>
       
             )}
       
-        </Formik>  
+        </Formik>
+ 
         </Wrapper>
         </>
 
