61fcefd618dfda72e3a40736d76e1e0adca66fa8
saadhafa <saad.811@icloud.com>
2021-05-08 21:24:31 +0000

    cookie forward to nextjs server when ssr

--- frontend-app/src/components/NavBar.tsx
+++ frontend-app/src/components/NavBar.tsx
@@ -1,57 +1,69 @@
-import { Box, Flex,Text } from '@chakra-ui/layout'
-import { Link } from '@chakra-ui/react'
-import React from 'react'
+import { Box, Flex, Text } from "@chakra-ui/layout";
+import { Link } from "@chakra-ui/react";
+import React from "react";
 
-import  NextLink from 'next/link'
-import { useLogoutMutation, useMeQuery } from '../generated/graphql'
-import { isServer } from '../util/isServer'
+import NextLink from "next/link";
+import { useLogoutMutation, useMeQuery } from "../generated/graphql";
+import { isServer } from "../util/isServer";
 
+const NavBar: React.FC = () => {
+  const [{ data }] = useMeQuery({
+    pause: isServer(),
+  });
+  const [, logout] = useLogoutMutation();
 
+  let body = null;
 
-const NavBar:React.FC = () => {
-const [{data,fetching}] = useMeQuery(
-    {
-        pause:isServer()
-    
-    })
-const [,logout] = useLogoutMutation()
-
-let body = null 
-
- if (!data?.me){
-    body =  (
-        <>
-<NextLink href="/login">
-        <Link ml={4} color="white">Login</Link>
+  if (!data?.me) {
+    body = (
+      <>
+        <NextLink href="/login">
+          <Link ml={4} color="white">
+            Login
+          </Link>
         </NextLink>
         <NextLink href="/register">
-        <Link ml={4} color="white">register</Link>
+          <Link ml={4} color="white">
+            register
+          </Link>
         </NextLink>
-        </>
-    )   
-}else{
+      </>
+    );
+  } else {
     body = (
-        <Box>
-              <Text  display="inline" ml={4} fontWeight="bolder" color="black">{data.me.username}</Text>
-              <NextLink href="/create-post">
-                    <Link ml={4} color="white">Create Post</Link>
-            </NextLink>
-              <Link  onClick={() => {
-                  logout()
-              }} ml={5} color="white">Logout</Link>
-        </Box> 
-    )
-}
+      <Box>
+        <Text display="inline" ml={4} fontWeight="bolder" color="black">
+          {data.me.username}
+        </Text>
+        <NextLink href="/create-post">
+          <Link ml={4} color="white">
+            Create Post
+          </Link>
+        </NextLink>
+        <Link
+          onClick={() => {
+            logout();
+          }}
+          ml={5}
+          color="white"
+        >
+          Logout
+        </Link>
+      </Box>
+    );
+  }
 
-
-    return(
-    <Flex position='sticky' zIndex={3} top="0" bg="tomato" justifyContent="flex-end">
-        <Box p={4} >
-        {body}
-        </Box>
+  return (
+    <Flex
+      position="sticky"
+      zIndex={3}
+      top="0"
+      bg="tomato"
+      justifyContent="flex-end"
+    >
+      <Box p={4}>{body}</Box>
     </Flex>
-    )
-}
+  );
+};
 
-
-export default NavBar
+export default NavBar;
